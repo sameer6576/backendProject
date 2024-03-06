4d91240a33c1e238cf970142be59a815105bb208
sameer6576 <77893868+sameer6576@users.noreply.github.com>
2024-03-06 16:47:56 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,20 +11,17 @@ app.use(
   })
 );
 
-app.use(
-  express.json({
-    limit: "16kb",
-  })
-);
-
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  })
-);
-
-app.use(express.static("public"));  
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
 export { app };
--- src/db/index.js
+++ src/db/index.js
@@ -1,18 +1,15 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB = async () => {
-  try {
-    const connnectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
-    console.log(
-      `\n MongoDB connected !! DB HOST : ${connnectionInstance.connection.host}`
-    );
-  } catch (error) {
-    console.log("Error in connectDB, MongoDB connection failed", error);
-    process.exit(1);
-  }
-};
 
-export default connectDB;
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
